ae69c700d5f4b751cbe086a372b80c6e147cffcb
RaveendraTiyyagura <121993935+RAVEENTI@users.noreply.github.com>
2023-01-04 19:21:03 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 'Breakfast Menu'
 
-'Omega 3 & Bluberry Oatmeal
+'Omega 3 & Bluberry Oatmeal'
 
-Kale, Spinach & Rocket Snoothhie
+'Kale, Spinach & Rocket Snoothhie'
 
-Hard-Boiled Free-Range Egg'
+'Hard-Boiled Free-Range Egg'
